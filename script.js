4d96b3e1b6a0a18c1b2099e3e36cfc0b7c5ef371
Jarrod Taylor <jarrodtaylor@icloud.com>
2017-03-19 17:30:46 -0400

    Makes the stars interactive

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var swapAvatar = function() {
   var num = Math.floor(Math.random() * 9) * 12.5
-  document.getElementById('avatar').style.backgroundPosition = num + "% 0%";
+  document.getElementById('avatar').style.backgroundPosition = num + "% 0%"
   setTimeout(swapAvatar, 3000)
 }
 
@@ -9,4 +9,18 @@ setTimeout(swapAvatar, 3000)
 var goto = function(url) {
   window.location.href = url
   return false
+}
+
+var setStars = function(starClass) {
+  document.getElementById('stars').className = starClass
+}
+
+var thanks = function(num) {
+  switch(num) {
+    case 1: confirm("Seriously, that's it?"); break
+    case 2: confirm("Are you sure? That's only two stars."); break
+    case 3: confirm("Try again. And a little to the right this time."); break
+    case 4: confirm("That's almost enough. How about one more?"); break
+    default: alert("Awesome!")
+  }
 }
