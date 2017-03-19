ac3c2e12a027e5de9e63693f49cbc042fc0216db
Jarrod Taylor <jarrodtaylor@icloud.com>
2017-03-19 14:07:40 -0400

    Adds content and design

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Welcome!")
+var swapAvatar = function() {
+  var num = Math.floor(Math.random() * 9)
+  console.log(num)
+  document.getElementById('avatar').style.backgroundImage = "url('images/avatars/"+num+".jpg')";
+  setTimeout(swapAvatar, 3000)
+}
+
+setTimeout(swapAvatar, 3000)
+
+var goto = function(url) {
+  window.location.href = url
+  return false
+}
